33c9c69164448ac0d65b734162b212bf8fb9ec34
Tayana <skorokhodova.t@gmail.com>
2019-03-20 13:46:57 +0300

    Update level1_1.py
    No import math.

--- level1_1.py
+++ level1_1.py
@@ -1,5 +1,13 @@
-import math
-
 def squirrel(N):
-    factorial = str(math.factorial(N))
-    return int(factorial[0])
+    
+    if N == 0:
+        return 0
+    else:
+        factorial = 1
+        for i in range(2, N+1):
+            factorial *= i
+        
+        factorial_string = str(factorial)
+    
+        return int(factorial_string[0])
+
